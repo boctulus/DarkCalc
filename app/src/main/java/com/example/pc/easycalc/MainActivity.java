da5529a9eb7a5c3992d4b57e30298d07037bdc7b
Boctulus <boctulus@gmail.com>
2017-12-25 01:16:10 -0300

    Clipboard

--- app/src/main/java/com/example/pc/easycalc/MainActivity.java
+++ app/src/main/java/com/example/pc/easycalc/MainActivity.java
@@ -1,28 +1,22 @@
 package com.example.pc.easycalc;
 
-import android.annotation.SuppressLint;
-import android.app.ActionBar;
-import android.graphics.LightingColorFilter;
-import android.graphics.PorterDuff;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.ViewGroup;
-import android.view.Window;
+import android.view.ViewTreeObserver;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.GridLayout;
 import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -34,31 +28,73 @@ public class MainActivity extends AppCompatActivity {
     private char op1;
     private char op2;
 
+    private  ClipboardManager clipboardManager;
+
+    static final String TAG = "DEBUG";
+
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+
+        clipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+
 
         //to remove "information bar" above the action bar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         //to remove the action bar (title bar)
+
         getSupportActionBar().hide();
 
 
-        AutoResizeEditText display = findViewById(R.id.resDisplay);
-        display.setText("");
-        display.setEnabled(true);
-        display.setFocusableInTouchMode(true);
-        display.setFocusable(true);
-        display.setEnableSizeCache(false);
-        display.setMovementMethod(null);
-        // can be added after layout inflation; it doesn't have to be fixed
-        // value
-        display.setMaxHeight(330);
+        /* Evito colapsen los displays */
+
+        final LinearLayout display = findViewById(R.id.display);
+
+        display.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+
+                /* Toca apagar el listener para evitar se llame cada vez */
+                display.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+
+                display.setMinimumHeight(display.getHeight());
+            }
+        });
+
+        final LinearLayout hot_display_cont = findViewById(R.id.hotDisplayContainer);
+
+        hot_display_cont.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+
+                /* Toca apagar el listener para evitar se llame cada vez */
+                hot_display_cont.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+
+                hot_display_cont.setMinimumHeight(hot_display_cont.getHeight());
+            }
+        });
+
+
+        EditText res_display = findViewById(R.id.resDisplay);
+
+        if (res_display.getText().toString() == "EASY CALC")
+            res_display.setText("");
+
+        res_display.setEnabled(true);
+        res_display.setFocusableInTouchMode(true);
+        res_display.setFocusable(true);
+        //res_display.setEnableSizeCache(false);
+        //res_display.setMovementMethod(null);
+        //res_display.setMaxHeight(330);
+
+
+        // Register the context menu to the the EditText
+        registerForContextMenu(res_display);
 
 
         LinearLayout root = (LinearLayout) findViewById(R.id.root);
@@ -79,7 +115,7 @@ public class MainActivity extends AppCompatActivity {
 
                 boton.setOnClickListener(new View.OnClickListener(){
                     public void onClick(View v){
-                        TextView display = findViewById(R.id.resDisplay);
+                        EditText display = findViewById(R.id.resDisplay);
                         Button b = (Button) v;
 
                         String newEntry = b.getText().toString();
@@ -174,10 +210,126 @@ public class MainActivity extends AppCompatActivity {
 
             }
 
+
+            disableSoftInput((EditText) findViewById(R.id.hotDisplay));
+            disableSoftInput((EditText) findViewById(R.id.resDisplay));
+
+            //getWindow().setFlags(WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM, WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
+
         }
 
 
+
     } // end fn
+
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+
+        //AutoResizeEditText display = findViewById(R.id.resDisplay);
+        //savedInstanceState.putString("res",display.getText().toString());
+        // etc.
+
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+
+        super.onRestoreInstanceState(savedInstanceState);
+
+        //String res = savedInstanceState.getString("res");
+        //AutoResizeEditText display = findViewById(R.id.resDisplay);
+        //display.setText(res);
+
+    }
+
+
+    /**
+     * Disable soft keyboard from appearing, use in conjunction with android:windowSoftInputMode="stateAlwaysHidden|adjustNothing"
+     * @param editText
+     */
+    public static void disableSoftInput(EditText editText) {
+        if (Build.VERSION.SDK_INT >= 11) {
+            editText.setRawInputType(InputType.TYPE_CLASS_TEXT);
+            editText.setTextIsSelectable(true);
+        } else {
+            editText.setRawInputType(InputType.TYPE_NULL); // none
+            editText.setFocusable(true);
+        }
+
+
+    }
+
+    /* Menu contextual flotante */
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.itemcopy:
+                copyText();
+                return true;
+            case R.id.itempaste:
+                pasteText();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.contextmenu, menu);
+
+
+        MenuItem paste_action = menu.findItem(R.id.itempaste);
+        if (!(clipboardManager.hasPrimaryClip())) {
+            paste_action.setEnabled(false);
+        }else{
+            paste_action.setEnabled(true);
+        }
+
+
+    }
+
+
+    /* Clipboard */
+
+    public void copyText(){
+        EditText display = findViewById(R.id.resDisplay);
+
+        //save data to clipboard
+        ClipData clipData = ClipData.newPlainText("RESULTADO", display.getText().toString());
+        clipboardManager.setPrimaryClip(clipData);
+
+    }
+
+    public void pasteText(){
+        EditText display = findViewById(R.id.resDisplay);
+
+        if (!clipboardManager.hasPrimaryClip())
+          return;
+
+        try{
+            ClipData clipData=clipboardManager.getPrimaryClip();
+
+            int curPos=display.getSelectionStart();
+            String textToPaste=clipData.getItemAt(0).getText().toString();
+            String oldText=display.getText().toString();
+            String textBeforeCursor=oldText.substring(0,curPos);
+            String textAfterCursor=oldText.substring(curPos);
+            String newText=textBeforeCursor+textToPaste+textAfterCursor;
+
+            display.setText(newText);
+
+        }catch(NullPointerException e){}
+
+
+
+    }
 
 
 
